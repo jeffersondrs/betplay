13b2d6d12f22b64901a257ad07facd5337d74944
jeffersondrs <jeffersonduarte@id.uff.br>
2023-01-20 19:29:16 -0300

    value saving in a object, input pix add

--- src/components/Form/Form.component.tsx
+++ src/components/Form/Form.component.tsx
@@ -1,11 +1,57 @@
 import Input from "../Input/Input.component";
+import { useState } from "react";
+
+interface Object {
+  name: string;
+  phone: string;
+  chavePix: string;
+}
 
 export default function Form() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [chavePix, setChavePix] = useState("");
+
+  const nameInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const phoneInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhone(e.target.value);
+  };
+
+  const chavePixInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChavePix(e.target.value);
+  };
+
+  const object: Object = {
+    name: name,
+    phone: phone,
+    chavePix: chavePix,
+  };
+
+  console.log(object);
+
   return (
     <>
       <div>Formulário</div>
-      <Input type="text" placeholder="Digite seu nome" labelValue="Nome" />
-      <Input type='tel' placeholder='Digite seu telefone' labelValue='Telefone' />
+      <Input
+        onChange={nameInputChange}
+        type="text"
+        placeholder="Digite seu nome"
+        labelValue="Nome"
+      />
+      <Input
+        onChange={phoneInputChange}
+        type="tel"
+        placeholder="Digite seu telefone"
+        labelValue="Telefone"
+      />
+      <Input
+        onChange={chavePixInputChange}
+        type="text"
+        placeholder="Digite sua chave pix"
+        labelValue="Chave Pix"
+      />
     </>
   );
 }
--- src/components/Input/Input.component.tsx
+++ src/components/Input/Input.component.tsx
@@ -1,29 +1,13 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 
 interface InputProps {
   type: string;
   placeholder: string;
   labelValue: string;
-}
-
-interface InputObject {
-  name: string;
-  phone: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function Input(props: InputProps) {
-
-  const [value, setValue] = useState<InputObject>({
-    name: "",
-    phone: "",
-  });
-
-  function handleChange(event: ChangeEvent<HTMLInputElement>) {
-    setValue({ name: event.target.value, phone: event.target.value });
-  }
-
-  console.log(value);
-
   return (
     <div className="text-sky-600 flex flex-col justify-evenly bg-gray-200 w-64 p-5">
       <label>{props.labelValue}</label>
@@ -31,8 +15,7 @@ export default function Input(props: InputProps) {
         className="outline-none border-2 border-gray-300 rounded-md p-1"
         type={props.type}
         placeholder={props.placeholder}
-        onChange={handleChange}
-        
+        onChange={props.onChange}
       />
     </div>
   );
