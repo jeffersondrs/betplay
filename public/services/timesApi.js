db6362521330388c870a9e18d5cc8dbdd8e3dec1
Jefferson Duarte Ramos dos Santos <e.jefferson.santos@mpt.mp.br>
2023-01-24 16:47:49 -0300

    api local add

---
+++ public/services/timesApi.js
@@ -0,0 +1,62 @@
+export const times = [
+  {
+    id: 1,
+    time: "Flamengo",
+    city: "Rio de Janeiro",
+  },
+  {
+    id: 2,
+    time: "São Paulo",
+    city: "São Paulo",
+  },
+  {
+    id: 3,
+    time: "Palmeiras",
+    city: "São Paulo",
+  },
+  {
+    id: 4,
+    time: "Grêmio",
+    city: "Porto Alegre",
+  },
+  {
+    id: 5,
+    time: "Internacional",
+    city: "Porto Alegre",
+  },
+  {
+    id: 6,
+    time: "Atlético Mineiro",
+    city: "Belo Horizonte",
+  },
+  {
+    id: 7,
+    time: "Santos",
+    city: "Santos",
+  },
+  {
+    id: 8,
+    time: "Corinthians",
+    city: "São Paulo",
+  },
+  {
+    id: 9,
+    time: "Cruzeiro",
+    city: "Belo Horizonte",
+  },
+  {
+    id: 10,
+    time: "Fluminense",
+    city: "Rio de Janeiro",
+  },
+  {
+    id: 11,
+    time: "Botafogo",
+    city: "Rio de Janeiro",
+  },
+  {
+    id: 12,
+    time: "Vasco da Gama",
+    city: "Rio de Janeiro",
+  },
+];
--- src/components/ApiRequest/Api.component.tsx
+++ src/components/ApiRequest/Api.component.tsx
@@ -1,38 +1,21 @@
-import { useState, useEffect } from "react";
-interface BetUser {
-  _id: number;
-  name: string;
-  phone: number;
-  chavePix: string;
-}
-
-type BetUsers = BetUser[];
+import { times } from "../../../public/services/timesApi";
 
 export default function Request() {
-  const [data, setData] = useState<BetUsers>();
-
-  useEffect(() => {
-    fetch("https://betplay.onrender.com/api")
-      .then((response) => response.json())
-      .then((json) => setData(json.data.betUsers));
-  }, [data]);
-
   return (
-    <div className="flex flex-row flex-wrap justify-center items-center">
-      {data?.map((name) => {
+    <div>
+      {times.map((name: any) => {
         return (
           <div
-            key={name._id}
+            key={name.id}
             className="m-2 p-1 bg-sky-200 w-56 h-36 justify-evenly items-center flex flex-col"
           >
-            <p className="bg-green-700 text-center w-40 text-white">
-              {name.name}
+            <p className="bg-sky-700 text-center w-full text-white">
+              Time: {name.time}
             </p>
-            <p className="bg-green-800 text-white text-center w-40">
-              {name.phone}
-            </p>
-            <p className="bg-green-900 text-white text-center w-40">
-              {name.chavePix}
+
+            <p className="bg-sky-900 text-white text-center w-full">
+              <strong>Cidade: </strong>
+              {name.city}
             </p>
           </div>
         );
