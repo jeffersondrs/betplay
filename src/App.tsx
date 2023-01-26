de12e1d5327dcdeb934cbdd9ce85439499aad1f6
Jefferson Duarte Ramos dos Santos <e.jefferson.santos@mpt.mp.br>
2023-01-26 16:32:44 -0300

    css update, match component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,78 @@
 import "./App.css";
 import Form from "./components/Form/Form.component";
-import Request from "./components/ApiRequest/Api.component";
+import Match from "./components/Matchs/Matchs.components";
+import flamengo from "../assets/escudo.png";
+import vasco from "../assets/vasco.png";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <div className="flex flex-col ">
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+      </div>
       <div>
-        <Request />
+        <Form />
+      </div>
+      <div>
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
+        <Match
+          timecasa="Flamengo"
+          escudocasa={flamengo}
+          timefora="Vasco"
+          escudofora={vasco}
+        />
       </div>
     </div>
   );
--- src/components/Input/Input.component.tsx
+++ src/components/Input/Input.component.tsx
@@ -9,8 +9,8 @@ interface InputProps {
 
 export default function Input(props: InputProps) {
   return (
-    <div className="text-black flex flex-col justify-evenly bg-green-200/50 w-96 p-5">
-      <label className="text-lg uppercase w-full text-start font-semibold text-green-900 ">{props.labelValue}</label>
+    <div className="text-black flex flex-col justify-evenly w-64 p-5">
+      <label className="text-sm uppercase w-full text-start font-semibold text-green-900 ">{props.labelValue}</label>
       <input
         className="outline-none border-2 border-green-500 rounded-sm p-1 placeholder:text-green-400"
         type={props.type}
