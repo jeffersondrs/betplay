fe5b20708c80a3842bbc3cff1bccca8a99829436
jeffersondrs <jeffersonduarte@id.uff.br>
2023-02-15 19:34:10 -0300

    add component checkout paypal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,14 @@ import "./App.css";
 import Form from "./components/Form/Form.component";
 import ChoiceTimes from "./components/ApiRequest/ChoiceTime";
 import { jogos } from "./api/services/timesApi";
+import Checkout from './components/Paypal/Checkout';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+const initialOptions = {
+  "client-id": "ARnnF4S56wNt3sxoBzRMvUYfz3h_l1JCAF_w4sDCvIvpTueM8U3DbAJxGS0f4LMRSO_Xa0eEBTOOsxhY",
+  currency: "USD",
+  intent: "capture",
+};
 
 function App() {
   return (
@@ -19,6 +27,9 @@ function App() {
           );
         })}
       </div>
+      <PayPalScriptProvider options={initialOptions}>
+        <Checkout/>
+    </PayPalScriptProvider>
     </div>
   );
 }
